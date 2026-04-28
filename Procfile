4420f32396413f497a676504e269f50768ef19e4
Macintosh <macintosh@Macintoshs-MacBook-Pro.local>
2026-04-28 20:13:43 +0100

    railway deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: uvicorn dune:app --host 0.0.0.0 --port $PORT
+\git
--- main.py
+++ main.py
@@ -5,11 +5,24 @@ from fastapi import FastAPI
 app = FastAPI() # Define a route for the root URL
 @app.get("/")
 def home():
-    return {"message": "Welcome to the FastAPI application!"}
+    return {"message": "Welcome to the FastAPI application!",
+            "swagger ui": "/docs",
+            "status": "success",
+            "endpoints": ["/about", "/csv"]
+            }
 
 @app.get("/about")
 def about():
     return {
         "course": "Python Full Stack developer", 
-        "instructor": "Josh"
+        "instructor": "Josh",
+        "description": "Learn Python programming from scratch to building full-stack applications"
     }
+
+
+@app.get("/csv")
+def csv():
+    csv_content = "name,age,city\nAlice,30,New York\nBob,25,Los Angeles\nCharles,35,Chicago"
+    return {"csv_data": csv_content}
+
+
